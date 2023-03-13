d480ad4067624724994926c0f1f72e948456b7ca
Shafayat Hossain Bayezid <shafayat.h.b@gmail.com>
2023-03-13 23:41:49 +0600

    practice

--- Class Practice/OOP/livetestMod5.php
+++ Class Practice/OOP/livetestMod5.php
@@ -1,21 +1,21 @@
 <?php
 $file_name = "ostad.txt";
-$file = fopen($file_name, "r+");
+$file = fopen( $file_name, "r+" );
 
 // If the file does not exist, create it
-if (file_exists($file_name) != true) {
-  $file = fopen($file_name, "w");
+if ( file_exists( $file_name ) != true ) {
+    $file = fopen( $file_name, "w" );
 }
 
 // Read the contents of the file
-$file_contents = fread($file, filesize($file_name));
+$file_contents = fread( $file, filesize( $file_name ) );
 
 // Add the current date and time to the contents of the file
-$date_time = date("Y-m-d H:i:s");
+$date_time = date( "Y-m-d H:i:s" );
 $file_contents .= "\n$date_time";
 
 // Write the new contents back to the file
-fseek($file, 0);
-fwrite($file, $file_contents);
-fclose($file);
+fseek( $file, 0 );
+fwrite( $file, $file_contents );
+fclose( $file );
 ?>
