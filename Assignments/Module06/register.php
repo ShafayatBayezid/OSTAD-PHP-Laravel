c6efb27a75b1746752035b1649d85b1acabdf863
unknown <shafayat.h.b@gmail.com>
2023-03-13 15:19:07 +0600

    Assignments on Module 06

--- Assignments/Module06/register.php
+++ Assignments/Module06/register.php
@@ -1,31 +1,32 @@
 <?php
 // Validate form inputs
-if (empty($_POST['name']) || empty($_POST['email']) || empty($_POST['password'])) {
-	die('Please fill out all fields.');
+if ( empty( $_POST['name'] ) || empty( $_POST['email'] ) || empty( $_POST['password'] ) ) {
+    die( 'Please fill out all fields.' );
 }
 
-if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-	die('Invalid email format.');
+if ( !filter_var( $_POST['email'], FILTER_VALIDATE_EMAIL ) ) {
+    die( 'Invalid email format.' );
 }
 
 // Save profile picture to server
 $uploadsDir = 'uploads/';
-$pictureName = uniqid() . '-' . $_FILES['picture']['name'];
+$currentDateTime = date( 'Y-m-d--H-i-s' );
+$pictureName = $currentDateTime . '--' . uniqid() . '--' . $_FILES['picture']['name'];
 $picturePath = $uploadsDir . $pictureName;
 
-if (!move_uploaded_file($_FILES['picture']['tmp_name'], $picturePath)) {
-	die('Failed to upload profile picture.');
+if ( !move_uploaded_file( $_FILES['picture']['tmp_name'], $picturePath ) ) {
+    die( 'Failed to upload profile picture.' );
 }
 
 // Save user data to CSV file
-$usersFile = fopen('users.csv', 'a');
-fputcsv($usersFile, array($_POST['name'], $_POST['email'], $pictureName));
-fclose($usersFile);
+$usersFile = fopen( 'users.csv', 'a' );
+fputcsv( $usersFile, array( $_POST['name'], $_POST['email'], $pictureName ) );
+fclose( $usersFile );
 
 // Start new session and set cookie
 session_start();
 $_SESSION['name'] = $_POST['name'];
 
 // Redirect to success page
-header('Location: success.php');
+header( 'Location: success.php' );
 ?>
