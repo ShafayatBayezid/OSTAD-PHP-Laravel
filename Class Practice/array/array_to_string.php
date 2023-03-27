1c295b10a80511ee452e8f440fcffcb8e037e5d9
unknown <shafayat.h.b@gmail.com>
2023-03-27 10:41:53 +0600

    array to json

---
+++ Class Practice/array/array_to_string.php
@@ -0,0 +1,31 @@
+<?php
+$students = [
+  [
+      'fname' => 'Shafayat',
+      'lname' => 'Hossain',
+      'Age'   => '27',
+  ],
+  [
+      'fname' => 'Ahmed',
+      'lname' => 'Bayezid',
+      'Age'   => '29',
+  ],
+  [
+      'fname' => 'Sakib',
+      'lname' => 'Sadman',
+      'Age'   => '25',
+  ],
+];
+
+// print_r($students);
+
+// $serializedData = serialize($students);
+// echo $serializedData;
+
+// $unserializedData = unserialize($serializedData);
+// echo $unserializedData;
+
+$jsonEncoded = json_encode($students);
+// echo $jsonEncoded;
+$jsonDecoded = json_decode($jsonEncoded, true); // true means, it will decode as array. without using true, it will decode as json object.
+print_r($jsonDecoded);
