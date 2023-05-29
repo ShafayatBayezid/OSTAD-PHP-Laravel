7328d4e3abd19965aceca8380672f40224bea617
unknown <shafayat.h.b@gmail.com>
2023-05-29 23:30:22 +0600

    assignment on module 15

---
+++ Assignments/module15/resources/views/Layout/header.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-sm bg-secondary navbar-light ">
+  <div class="container">
+    <!-- Brand/logo -->
+    <a class="navbar-brand text-white" href="/">Logo</a>
+    
+    <!-- Links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link text-white" href="/">Welcome</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="/home">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="/dashboard">Dashboard</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="/signup">Sign up</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="/contact">Contact us</a>
+      </li>
+    </ul>
+  </nav>
+</div>
