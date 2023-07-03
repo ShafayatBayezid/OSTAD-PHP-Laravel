db7eb81d37891d151c2ccbadcc509f618e02b9c3
unknown <shafayat.h.b@gmail.com>
2023-07-03 23:54:08 +0600

    Assignments on Module 18

--- Assignments/Module18/resources/views/components/navbar.blade.php
+++ Assignments/Module18/resources/views/components/navbar.blade.php
@@ -7,7 +7,7 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
-        <li class="nav-item"><a class="nav-link" href="{{url('/postData')}}">PostData</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{url('/postData')}}">Posts</a></li>
         <li class="nav-item"><a class="nav-link" href="{{url('/categories')}}">Categories</a></li>
       </ul>
     </div>
